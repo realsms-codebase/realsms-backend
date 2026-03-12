e42062bd7230530a7a7fcdccf6871bbaa9cea6d7
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-12 10:30:53 +0100

    Update Support.js

--- src/models/Support.js
+++ src/models/Support.js
@@ -1,3 +1,34 @@
+// const mongoose = require("mongoose");
+
+// const supportSchema = new mongoose.Schema(
+//   {
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+
+//     message: {
+//       type: String,
+//       required: true,
+//     },
+
+//     sender: {
+//       type: String,
+//       enum: ["user", "admin"],
+//       required: true,
+//     },
+
+//     read: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Support", supportSchema);
+
 const mongoose = require("mongoose");
 
 const supportSchema = new mongoose.Schema(
@@ -16,7 +47,7 @@ const supportSchema = new mongoose.Schema(
     sender: {
       type: String,
       enum: ["user", "admin"],
-      required: true,
+      default: "user",
     },
 
     read: {
