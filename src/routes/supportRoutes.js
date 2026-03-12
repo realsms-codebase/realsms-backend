45556f491aa49c3bcda6731f41ea7d5a02c4629d
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-12 10:29:13 +0100

    Update supportRoutes.js

--- src/routes/supportRoutes.js
+++ src/routes/supportRoutes.js
@@ -8,24 +8,17 @@
 //   getAdminMessages,
 // } = require("../controllers/supportController");
 
-// const { protect, adminOnly } = require("../middleware/adminAuthMiddleware");
-// // const { protectUser } = require("../middleware/authMiddleware");
+// const { protectSupportUser, protectSupportAdmin } = require("../middleware/supportAuthMiddleware");
 
+// // USER SUPPORT ROUTES
+// router.post("/send", protectSupportUser, sendMessage);
+// router.get("/user", protectSupportUser, getUserMessages);
 
-// // user send message
-// router.post("/send", sendMessage);
-
-// // user fetch messages
-// router.get("/user", getUserMessages);
-
-// // admin fetch all chats
-// router.get("/admin", protect, adminOnly, getAdminMessages);
-
-// // admin reply
-// router.post("/reply", protect, adminOnly, adminReply);
+// // ADMIN SUPPORT ROUTES
+// router.get("/admin", protectSupportAdmin, getAdminMessages);
+// router.post("/reply", protectSupportAdmin, adminReply);
 
 // module.exports = router;
-
 
 const express = require("express");
 const router = express.Router();
@@ -35,16 +28,26 @@ const {
   adminReply,
   getUserMessages,
   getAdminMessages,
+  getUnreadMessages,
+  markMessagesAsRead,
 } = require("../controllers/supportController");
 
-const { protectSupportUser, protectSupportAdmin } = require("../middleware/supportAuthMiddleware");
+const {
+  protectSupportUser,
+  protectSupportAdmin,
+} = require("../middleware/supportAuthMiddleware");
+
 
 // USER SUPPORT ROUTES
 router.post("/send", protectSupportUser, sendMessage);
 router.get("/user", protectSupportUser, getUserMessages);
 
+
 // ADMIN SUPPORT ROUTES
 router.get("/admin", protectSupportAdmin, getAdminMessages);
+router.get("/admin/unread", protectSupportAdmin, getUnreadMessages);
 router.post("/reply", protectSupportAdmin, adminReply);
+router.put("/admin/read/:userId", protectSupportAdmin, markMessagesAsRead);
+
 
 module.exports = router;
