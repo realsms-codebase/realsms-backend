7a5697161b02a2669b889cbbfdbee0eeb8282417
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-12 10:27:12 +0100

    Update supportController.js

--- src/controllers/supportController.js
+++ src/controllers/supportController.js
@@ -1,6 +1,5 @@
 const Support = require("../models/Support");
 
-
 // USER SEND MESSAGE
 exports.sendMessage = async (req, res) => {
   try {
@@ -46,7 +45,6 @@ exports.getUserMessages = async (req, res) => {
   }
 };
 
-
 // ADMIN GET ALL CONVERSATIONS
 exports.getAdminMessages = async (req, res) => {
   try {
@@ -55,6 +53,20 @@ exports.getAdminMessages = async (req, res) => {
       .sort({ createdAt: -1 });
 
     res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// GET UNREAD SUPPORT MESSAGES
+exports.getUnreadMessages = async (req, res) => {
+  try {
+    const unread = await Support.find({
+      sender: "user",
+      read: false,
+    });
+
+    res.json(unread);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
