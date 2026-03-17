c9e8530631591b62ee709ac800200037bf0d49a8
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-17 13:08:25 +0100

    Update transactionsRoutes.js

--- src/routes/transactionsRoutes.js
+++ src/routes/transactionsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getUserTransactionStats } = require("../controllers/transactionController");
+const { getUserTransactionStats, getUserTransactions, } = require("../controllers/transactionController");
 const { protect } = require("../middleware/authMiddleware");
 
 // GET /api/transactions/stats - get stats for logged-in user
