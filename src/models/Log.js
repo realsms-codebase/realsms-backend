c8ef11f6747d16a40f86833fd6fad8032c7cea77
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-27 06:09:44 +0100

    Create Log.js

---
+++ src/models/Log.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const logSchema = new mongoose.Schema(
+  {
+    platform: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, default: 0 },
+    type: { type: String, required: true },
+    details: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Log", logSchema);
