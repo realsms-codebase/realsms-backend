8da1a0be01880b4d30d714638e99440764879b4b
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-27 06:11:37 +0100

    Create logRoutes.js

---
+++ src/routes/logRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { createLog, getLogs } = require("../controllers/logController");
+
+router.post("/", createLog);
+router.get("/", getLogs);
+
+module.exports = router;
