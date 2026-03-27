44728aba57d7fc49c45d2f8526983af4711cc9e5
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-27 17:21:57 +0100

    Update logRoutes.js

--- src/routes/logRoutes.js
+++ src/routes/logRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createLog, getLogs } = require("../controllers/logController");
+const { createLog, getLogs, deleteLog, } = require("../controllers/logController");
 
 router.post("/", createLog);
 router.get("/", getLogs);
+router.delete("/log/:id", deleteLog); 
 
 module.exports = router;
