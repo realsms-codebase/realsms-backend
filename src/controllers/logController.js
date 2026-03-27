de7f051230132bcd3d668c8299ca3f07517b3874
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-03-27 06:10:44 +0100

    Create logController.js

---
+++ src/controllers/logController.js
@@ -0,0 +1,21 @@
+const Log = require("../models/Log");
+
+// CREATE LOG
+exports.createLog = async (req, res) => {
+  try {
+    const log = await Log.create(req.body);
+    res.status(201).json(log);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// GET ALL LOGS
+exports.getLogs = async (req, res) => {
+  try {
+    const logs = await Log.find().sort({ createdAt: -1 });
+    res.json(logs);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
