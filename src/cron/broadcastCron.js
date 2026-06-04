0c2e4e525b33cf4615bbdd43526dc723153ccd7b
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-06-04 09:08:32 -0500

    Update broadcastCron.js

--- src/cron/broadcastCron.js
+++ src/cron/broadcastCron.js
@@ -22,7 +22,7 @@
 // });
 
 import cron from "node-cron";
-import { sendBroadcastEmail } from "./controllers/adminController.js";
+import { sendBroadcastEmail } from "./controllers/broadcastController.js";
 
 // runs every day at 9am
 cron.schedule("0 9 * * *", async () => {
