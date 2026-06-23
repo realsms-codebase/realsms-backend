4256a9e37cabfd9c6ac5a13572197001e59a1bc5
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-06-23 13:50:43 +0100

    Update logRoutes.js

--- src/routes/logRoutes.js
+++ src/routes/logRoutes.js
@@ -1,36 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   createLog,
-//   getLogs,
-//   deleteLog,
-//   updateLog,
-//   buyLog,
-//   getLogOrders,
-// } = require("../controllers/logController");
-
-// const { protect } = require("../middleware/authMiddleware");
-// // =======================
-// // LOG CRUD
-// // =======================
-// router.post("/", protect, createLog);       // optional: only admin can create
-// router.get("/", getLogs);                   // public
-// router.put("/:id", protect, updateLog);     // optional: only admin
-// router.delete("/:id", protect, deleteLog);  // optional: only admin
-
-// // =======================
-// // BUY LOG
-// // =======================
-// router.post("/buy/:id", protect, buyLog);   // ✅ require login
-
-// // =======================
-// // ORDER HISTORY
-// // =======================
-// router.get("/orders", protect, getLogOrders); // ✅ require login
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
