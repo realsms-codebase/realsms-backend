673f2d40ed686c115c71298d34686dfcb9327b7b
realsms-codebase <smsreal608@gmail.com>
2026-07-02 07:41:21 +0100

    Update broadcastCron.js

--- src/cron/broadcastCron.js
+++ src/cron/broadcastCron.js
@@ -1,26 +1,3 @@
-// // cron/broadcastCron.js
-
-// import cron from "node-cron";
-// import { sendDailyBroadcast } from "../controllers/broadcastController.js";
-
-// // Runs every day at 9AM
-// cron.schedule("0 9 * * *", async () => {
-//   console.log("Running daily broadcast (50 emails)");
-
-//   await sendDailyBroadcast(
-//     {
-//       body: {
-//         subject: "Daily Update",
-//         message: "Hello from our platform",
-//       },
-//     },
-//     {
-//       json: console.log,
-//       status: () => ({ json: console.log }),
-//     }
-//   );
-// });
-
 import cron from "node-cron";
 import { sendBroadcastEmail } from "../controllers/broadcastController.js";
 
